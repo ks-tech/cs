3821ead1891d3be73fea9e674db2bf9c2c7a8fb6
jindw <jindw@xidea.org>
2012-05-10 01:01:41 +0800

    child selectors

--- test/run.js
+++ test/run.js
@@ -5,5 +5,5 @@ var rbs = require('jsi/test/run').rbs
 var setupCS = require('../lib/compiler/cs-filter').setupCS;
 setupCS(rbs,'/');
 
-var s = rbs.getContentAsBinary("/test/-webkit-test.css").toString();
-//console.log(s)
+var s = rbs.getContentAsBinary("/test/-ie6-test.css").toString();
+console.log('\ncontents:\n',s)
