16b14e60760a9939ec8eb681631c6553dd36d721
jindw <jindw@xidea.org>
2012-05-16 00:13:53 +0800

    runtime 重构，未调通

--- -ie6-index.js
+++ -ie6-index.js
@@ -4,3 +4,21 @@ require('./lib/nth');
 require('./lib/box');
 require('./lib/matrix');
 exports.CS = require('./lib/core').CS
+
+var FilterHelper = require('./filter').FilterHelper
+var vistTransition = require('./lib/runtime/transition').vistTransition
+var pollMap = CS.pollMap;
+pollMap.opacity = function(el,opacity,config){
+	FilterUtil(el).setOpacity(transform).update();
+}
+pollMap.transform = function(el,transform,config){
+	FilterHelper(el).setTransform(transform).update();
+}
+pollMap["cs-linear-gradient"]=function(el,gradient,config){
+	FilterHelper(el).setLinearGradient(gradient).update();
+}
+pollMap["box"]=function(el,mw){
+	
+}
+
+pollMap.transition = vistTransition
