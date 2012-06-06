7e39e14914034757658a5421a2501407fce8fd1b
jindw <jindw@xidea.org>
2012-06-06 17:59:41 +0800

    add jsconsole

--- index.js
+++ index.js
@@ -7,41 +7,49 @@ var setupCS = require('./lib/compiler/cs-filter').setupCS;
 setupJSRequire(rbs,'/static/');
 setupCS(rbs,'/');
 
-
-
-writeExample('static/cs.js')
-writeExample('example/test.css')
-writeExample('example/test.html')
-
-function writeExample(path){
-	try{
-		FS.readFileSync(Path.resolve(path));
-	}catch(e){
-		if(e.code == 'ENOENT'){
-			//需要部署示例
-			var dir = path.replace(/[^\/]+$/,'')
-			try{
-				FS.readFileSync(Path.resolve(dir));
-			}catch(e){
-				if(e.code == 'ENOENT'){
-					//需要创建目录
-					console.log('mkdir:',dir)	
-					FS.mkdirSync(dir);
-				}
+function checkExample(){
+	var statusMap = {};
+	for(var i = 0;i<arguments.length;i++){
+		var path = arguments[i];
+		var file = require.resolve('cs/'+path);
+		var expect = FS.readFileSync(file);
+		try{
+			var source = FS.readFileSync(Path.resolve(path));
+			if(source != expect){
+				statusMap[path] = [expect,1];
 			}
-			var file = require.resolve('cs/'+path);
-			var content = FS.readFileSync(file)
-			console.log('copy file:',path)
-			FS.writeFile(path, content);
+		}catch(e){
+			if(e.code == 'ENOENT'){
+				statusMap[path] = [expect,0];
+			}else{
+				throw e;
+			}
 		}
 	}
+	return statusMap;
 }
-if(process.platform == 'win32'){
-	var exec = require('child_process').exec;
-	var libpath = require.resolve('cs/lib/runtime/index.js');
-	var filepath = Path.resolve('./lib/runtime/index.js');
-	//console.log(libpath != filepath,libpath,filepath)
-	function openBlowser(){
+
+function writeExample(path,content){
+	//需要部署示例
+	var dir = path.replace(/[^\/]+$/,'')
+	try{
+		FS.readFileSync(Path.resolve(dir));
+	}catch(e){
+		if(e.code == 'ENOENT'){
+			//需要创建目录
+			console.log('mkdir:',dir)	
+			FS.mkdirSync(dir);
+		}
+	}
+	console.log('copy file:',path)
+	FS.writeFile(path, content);
+}
+var exec = require('child_process').exec;
+var libpath = require.resolve('cs/lib/runtime/index.js');
+var filepath = Path.resolve('./lib/runtime/index.js');
+//console.log(libpath != filepath,libpath,filepath)
+function openBlowser(){
+	if(process.platform == 'win32'){
 		var intv = setInterval(function(){
 			var ad = rbs.server.address()
 			if(ad){
@@ -50,44 +58,72 @@ if(process.platform == 'win32'){
 			}
 		},300)
 	}
-	if(libpath != filepath){//if start from cs project ignore browser;
-		openBlowser()
-	}else{
-		setTimeout(function(){
-			var readline = require('readline');
-			var rl = readline.createInterface(process.stdin, process.stdout);
-			rl.prompt();
-			rl.question('open your browser? (yes|no)', function(v) {
-				//console.log('answer',v)
-				if(/yes/i.test(v)){
-					console.log('try to open your blowser...')
-					openBlowser();
-				}
-				rl.setPrompt('js>');
-				rl.prompt();
-			});
-			rl.on('line', function(line) {
-				switch(line.trim()) {
-				case 'help':
-					console.log('exit!');
-					break;
-				default:
-					try{
-						var v = eval(line);
-						if(/^[\._\w]+$/.test(line)){
-							v = require('util').inspect(v,true);
-						}
-						console.log(v);
-					}catch(e){
-						console.log(e);
-					}
-				}
-				rl.setPrompt('js>');
-				rl.prompt();
-			}).on('close', function() {
-				process.exit(0);
-			});
-			//rl.close();
-		},500);
-	}
 }
+
+	
+
+setTimeout(function(){
+	var readline = require('readline');
+	var rl = readline.createInterface(process.stdin, process.stdout);
+
+	var exampleStatus = checkExample('static/cs.js','example/test.css','example/test.html');
+	function addQuestion(n){
+		var status_ = exampleStatus[n];
+		var message = status_[1]?' is changed. replace?':' is not exist. copy?';
+		delete exampleStatus[n];
+		rl.question('file '+n+message+' (yes|no)', function(v) {
+			//console.log('answer',v)
+			if(/yes/i.test(v)){
+				writeExample(n,status_[0]);
+			}
+			for(var n in exampleStatus){
+				addQuestion(n);
+				return;
+			}
+			if(libpath != filepath){//if start from cs project ignore browser;
+				openBlowser()
+				startJSConsole()
+			}else{
+				rl.question('open your browser? (yes|no)', function(v) {
+					//console.log('answer',v)
+					if(/yes/i.test(v)){
+						console.log('try to open your blowser...')
+						openBlowser();
+					}
+					startJSConsole()
+				});
+			}
+		});
+		
+	}
+	for(var n in exampleStatus){
+		addQuestion(n);
+		break;
+	}
+	function startJSConsole(){
+		rl.setPrompt('js>');
+		rl.prompt();
+		rl.on('line', function(line) {
+			switch(line.trim()) {
+			case 'help':
+				console.log('exit!');
+				break;
+			default:
+				try{
+					var v = eval(line);
+					if(/^[\._\w]+$/.test(line)){
+						v = require('util').inspect(v,true);
+					}
+					console.log(v);
+				}catch(e){
+					console.log(e);
+				}
+			}
+			rl.setPrompt('js>');
+			rl.prompt();
+		}).on('close', function() {
+			process.exit(0);
+		});
+	}
+	//rl.close();
+},500);
