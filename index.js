9cde503f6dcae4546234d0d0b0dd90b71b65607c
jindw <jindw@xidea.org>
2012-06-11 23:09:42 +0800

    fade support

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ var setupJSRequire = require('jsi').setupJSRequire;
 var setupCS = require('./lib/compiler/cs-filter').setupCS;
 var addExample = require('rbs/lib/server-ext').addExample;
 setupJSRequire(rbs,'/static/');
-setupCS(rbs,/\.css$/i,/\.html?$/i,'/static/');
+setupCS(rbs,/\.css$/i,/\.html?$/i);
+
+
 
 ['static/cs.js',
 'static/cs.htc','example/test.css','example/test.html'].forEach(function(path){
@@ -19,3 +21,18 @@ if(require.resolve('cs/lib/runtime/cs-exported.js') != Path.resolve('./lib/runti
 	addExample('static/cs.js',FS.readFileSync(require.resolve('cs/lib/runtime/cs-exported.js')));
 }
 startServer(rbs);
+
+exports.setHtcPath = function(path){
+	console.log('htc path is reset as: ',path,'\nhtc mimeType is required as: text/x-component')
+	rbs.config.cs.htcPath = path;
+	addExample(path.replace(/^\//,''),FS.readFileSync(require.resolve('cs/static/cs.htc')));
+	return exports;
+}
+exports.setScriptPath = function(path){
+	console.log('script path is reset as: ',path)
+	rbs.config.cs.scriptPath = path;
+	if(!path.match(/^https?\:\/\//)){
+		addExample(path.replace(/^\//,''),FS.readFileSync(require.resolve('cs/lib/runtime/cs-exported.js')));
+	}
+	return exports;
+}
--- test/index.js
+++ test/index.js
@@ -1,10 +1,11 @@
-var Path = require('path')
-process.chdir(Path.dirname(__dirname))
-
+process.chdir(__dirname)
 var rbs = require('jsi/test').rbs
 var setupCS = require('../lib/compiler/cs-filter').setupCS;
-setupCS(rbs,'/');
-
+exports.setHtcPath = function(path){
+	console.log('htc file is reset as: ',path,'\nhtc mimeType is required as: text/x-component')
+	rbs.config.cs.htcPath = path;
+}
+setupCS(rbs,/\.css$/,/\.html?$/);
 //var s = rbs.getContentAsBinary("/example/selector/-ie6-not.css").toString();
-var s = rbs.getContentAsBinary("/static/cs.js").toString();
+//var s = rbs.getContentAsBinary("/static/cs.js").toString();
 //console.log('\ncontents:\n',s.slice(-200))
