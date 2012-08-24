71419506f587803b8e14e3bfe7c71bdc999268d2
jindw <jindw@xidea.org>
2012-08-25 00:28:37 +0800

    update rbs

--- index.js
+++ index.js
@@ -5,104 +5,34 @@ var RBS = require('rbs/lib/rbs').RBS;
 var rbs = new RBS();
 var setupJSRequire = require('jsi').setupJSRequire;
 var setupCS = require('./lib/compiler').setupCS;
-var addExample = require('rbs/lib/server/server-ext').addExample;
 setupJSRequire(rbs,'/static/');
 setupCS(rbs,/\.css$/i,/\.html?$/i);
-
+var exampleMap = {}
 var config = JSON.parse(FS.readFileSync(require.resolve('cs/package.json')));
 
 console.log('welcome to test cs: '+config.version);
 
 ['index.html','index.css','default.css',
-'static/cs.js',
-'static/gitbug.js',
-'static/cs.htc','example/test.css','example/test.html'].forEach(function(path){
+	'static/cs.js',
+	'static/gitbug.js',
+	'static/cs.htc','example/test.css','example/test.html'].forEach(function(path){
 	var file = require.resolve('cs/'+path);
 	var expect = FS.readFileSync(file);
-	addExample(path,expect);
+	exampleMap[path]=expect;
 });
 if(require.resolve('cs/lib/runtime/cs-exported.js') != Path.resolve('./lib/runtime/cs-exported.js')){
-	addExample('static/cs.js',FS.readFileSync(require.resolve('cs/lib/runtime/cs-exported.js')));
+	exampleMap['static/cs.js']=FS.readFileSync(require.resolve('cs/lib/runtime/cs-exported.js'));
 }
-startServer(rbs);
-
-exports.setHtcPath = function(path){
-	console.log('htc path is reset as: ',path,'\nhtc mimeType is required as: text/x-component')
-	rbs.config.cs.htcPath = path;
-	addExample(path.replace(/^\//,''),FS.readFileSync(require.resolve('cs/static/cs.htc')));
-	return exports;
-}
-exports.setScriptPath = function(path){
-	console.log('script path is reset as: ',path)
-	rbs.config.cs.scriptPath = path;
-	if(!path.match(/^https?\:\/\//)){
-		if(path.indexOf('/static/')!=0){
-			var content = rbs.getContentAsBinary('/static/cs/lib/runtime/cs-exported.js').toString()
-		}else{
-			content = FS.readFileSync(require.resolve('cs/lib/runtime/cs-exported.js'));
-		}
-		addExample(path.replace(/^\//,''),content);
-	}
-	return exports;
+var testServer = startServer(rbs);
+require('./lib/server/export').init(testServer,exports);
+testServer.complete = function(){
+	var root = testServer.root;
+	var webURL = 'http://127.0.0.1:'+testServer.port+'/';
+	console.log('rbs test server is started success!!\nfile :\t'+root+
+			'\nurl :\t'+webURL );
+	require('./lib/server/example').init(testServer,exampleMap);
 }
 
-var querystring = require('querystring');  
-var http = require('http');  
-
-  
-exports.exportTo = function(output,options){
-	var postData = genPostData(options)
-	var options = {
-		host: '127.0.0.1',
-		port: 2012,
-		"user-agent":'nodejs',
-		'path':'/--export.zip',
- 		method: 'POST',
-		headers: {  
-			'Content-Type': 'application/x-www-form-urlencoded',  
-			'Content-Length': postData.length  
-		}  
-	};
-
-	var post = http.request(options,function(res){
-		console.log('STATUS: ' + res.statusCode);
-		console.log('HEADERS: ' + JSON.stringify(res.headers));
-		console.log('write to:',output)
-		var out = FS.createWriteStream(output);
-		res.pipe(out);
-		res.on('end',function(){
-			console.log('close output:',output)
-			//out.close()
-		})
-	});
-	post.on('error', function(e) {
-		console.dir(e)
-		console.log("error: "+e.message,'\n',postData,postData.length );
-	});
-//	post.on('data', function (chunk) {
-//		console.log('BODY: ' + chunk);
-//	});
-	post.write(postData);
-	post.end();
-}
-function genPostData(options){
-	if(options && options.branch){
-		var postData = {  
-		}; 
-		if(options.branch.css){
-			postData.cssBranch = true;
-		}
-		if(options.branch.js){
-			postData.jsBranch = true;
-		}
-		if(options.branch.jsi){
-			postData.jsiClosure = true;
-		}
-		return querystring.stringify(postData)
-	}else{
-		return "cssBranch=true"
-	}
-}
 //var s = rbs.getContentAsBinary("/-webkit-index.css").toString();//console.log(s);
 //var s = require('http').get("http://localhost:2012/rbs/test.php",function(res){
 //	console.log("Got response: " + res.statusCode);
